80c1a91e23cf26be364d258345a788257358b113
lc3lx <lc3lx@users.noreply.github.com>
2025-12-27 12:31:38 +0300

    Fix client address authorization - add ownership check

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Customer = require("../models/customerModel");
+const ApiError = require("../utils/apiError");
 
 // @desc  add  addresses
 // @route post/api/addresses
@@ -27,7 +28,20 @@ exports.addAdrress = asyncHandler(async (req, res, next) => {
 // @access private/protect/Customer
 
 exports.removeaddress = asyncHandler(async (req, res, next) => {
-  const customer = await Customer.findByIdAndUpdate(
+  // التحقق من أن العنوان ينتمي للمستخدم الحالي
+  const customer = await Customer.findOne({
+    _id: req.customer._id,
+    "addresses._id": req.params.addressId
+  });
+
+  if (!customer) {
+    return res.status(403).json({
+      status: "fail",
+      message: "You are not authorized to access this route"
+    });
+  }
+
+  const updatedCustomer = await Customer.findByIdAndUpdate(
     req.customer._id,
     {
       $pull: { addresses: { _id: req.params.addressId } },
@@ -39,8 +53,8 @@ exports.removeaddress = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({
     message: "addresse removed successfully",
-    result: customer.addresses.length,
-    data: customer.addresses,
+    result: updatedCustomer.addresses.length,
+    data: updatedCustomer.addresses,
   });
 });
 
@@ -79,6 +93,19 @@ exports.updateAddress = asyncHandler(async (req, res, next) => {
       district,
     } = req.body;
 
+    // التحقق من أن العنوان ينتمي للمستخدم الحالي
+    const customer = await Customer.findOne({
+      _id: req.customer._id,
+      "addresses._id": addressId
+    });
+
+    if (!customer) {
+      return res.status(403).json({
+        status: "fail",
+        message: "You are not authorized to access this route"
+      });
+    }
+
     const updateFields = {};
 
     if (alias) updateFields["addresses.$.alias"] = alias;
@@ -91,19 +118,19 @@ exports.updateAddress = asyncHandler(async (req, res, next) => {
     if (city) updateFields["addresses.$.city"] = city;
     if (district) updateFields["addresses.$.district"] = district;
 
-    const customer = await Customer.findOneAndUpdate(
+    const updatedCustomer = await Customer.findOneAndUpdate(
       { _id: req.customer._id, "addresses._id": addressId },
       { $set: updateFields },
       { new: true }
     );
 
-    if (!customer) {
+    if (!updatedCustomer) {
       return res.status(404).json({ message: "Customer or address not found" });
     }
 
     res.status(200).json({
       message: "Address updated successfully",
-      data: customer.addresses,
+      data: updatedCustomer.addresses,
     });
   } catch (error) {
     console.error("Error updating address:", error);
--- controllers/clientAddressController.js
+++ controllers/clientAddressController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const factory = require("./handlersFactory");
 const Adddress = require("../models/clientAddressModel");
+const ApiError = require("../utils/apiError");
 
 
 
@@ -14,7 +15,21 @@ exports.getcleintAddress = asyncHandler(async (req, res, next) => {
 // @desc    Get specific cleintAddress by id
 // @route   GET /api/cleintAddresss/:id
 // @access  protect/Admin-Manager-user
-exports.getoneCleintAddress = factory.getOne(Adddress);
+exports.getoneCleintAddress = asyncHandler(async (req, res, next) => {
+  // التحقق من أن العنوان ينتمي للمستخدم الحالي
+  const address = await Adddress.findOne({
+    _id: req.params.id,
+    customer: req.customer._id
+  });
+
+  if (!address) {
+    return next(
+      new ApiError(`لا يمكن العثور على العنوان`, 404)
+    );
+  }
+
+  res.status(200).json({ data: address });
+});
 
 
 
@@ -34,9 +49,47 @@ exports.createcleintAddress = asyncHandler(async (req, res, next) => {
 // @desc    Update specific cleintAddress
 // @route   PUT /api/cleintAddresss/:id
 // @access  protect/Admin-Manager-user
-exports.updatecleintAddress = factory.updateOne(Adddress);
+exports.updatecleintAddress = asyncHandler(async (req, res, next) => {
+  // التحقق من أن العنوان ينتمي للمستخدم الحالي
+  const address = await Adddress.findOne({
+    _id: req.params.id,
+    customer: req.customer._id
+  });
+
+  if (!address) {
+    return next(
+      new ApiError(`لا يمكن العثور على العنوان أو أنت غير مصرح لتعديله`, 403)
+    );
+  }
+
+  // تحديث العنوان
+  const updatedAddress = await Adddress.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json({ data: updatedAddress });
+});
 
 // @desc    Delete specific cleintAddress
 // @route   DELETE /api/cleintAddresss/:id
 // @access  protect/Admin-Manager-user
-exports.deletecleintAddress = factory.deleteOne(Adddress);
+exports.deletecleintAddress = asyncHandler(async (req, res, next) => {
+  // التحقق من أن العنوان ينتمي للمستخدم الحالي
+  const address = await Adddress.findOne({
+    _id: req.params.id,
+    customer: req.customer._id
+  });
+
+  if (!address) {
+    return next(
+      new ApiError(`لا يمكن العثور على العنوان أو أنت غير مصرح لحذفه`, 403)
+    );
+  }
+
+  // حذف العنوان
+  await Adddress.findByIdAndDelete(req.params.id);
+
+  res.status(200).json({ status: "success", message: "تم حذف العنوان بنجاح" });
+});
