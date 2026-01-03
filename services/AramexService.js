dff6a97717ec42bc87dd8fe70a93db37fe022962
lc3lx <lc3lx@users.noreply.github.com>
2026-01-03 15:42:07 +0300

    f

--- services/AramexService.js
+++ services/AramexService.js
@@ -162,7 +162,7 @@ exports.shipmentData = (
             Value: weight,
             Unit: "KG",
           },
-          DescriptionOfGoods: orderDescription,
+          DescriptionOfGoods: `orderDescription ,nationalAddress:${order.customer.nationalAddress}`,
           GoodsOriginCountry: "SA",
           NumberOfPieces: 6,
           ProductGroup: "DOM",
